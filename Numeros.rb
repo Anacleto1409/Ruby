ba64fca32737e33a984713b389406570803314e5
Gabriel Simões <gabrielsantossimoes10@gmail.com>
2024-02-25 21:56:14 +0000

    Ruby1

---
+++ Numeros.rb
@@ -0,0 +1,18 @@
+puts "Insira um número"
+x = gets.chomp.to_f
+puts "Insira outro número"
+y = gets.chomp.to_f
+
+soma = x + y
+subt = x - y
+mult = x * y
+divi = x / y
+
+puts "O resultado da soma é"
+puts soma
+puts "O resultado da subtração é"
+puts subt
+puts "O resultado da multiplicação é"
+puts mult
+puts "O resultado da divisão é"
+puts divi
---
+++ StringsVariaveis.rb
@@ -0,0 +1,10 @@
+puts "Insira seu nome"
+nome = gets.chomp
+puts "Insira sua idade"
+idade = gets.chomp.to_i
+
+#nome = "Gabriel"
+#idade = "21 anos."
+
+#puts "Olá Mundo!"
+puts "O meu mome é #{nome} e tenho #{idade}"
